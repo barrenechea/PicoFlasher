9ff7f0877d36b6e04ba649d96d7cba6561a6c983
Sebastian Barrenechea <sebastian@barrenechea.cl>
2025-04-11 00:24:10 -0400

    refactor: use pico-sdk 2.1.1 PICO_USE_FASTEST_SUPPORTED_CLOCK flag

--- main.c
+++ main.c
@@ -19,8 +19,6 @@
 #include <string.h>
 
 #include "bsp/board.h"
-#include "hardware/vreg.h"
-#include "hardware/clocks.h"
 #include "pico/stdlib.h"
 #include "pico/bootrom.h"
 
@@ -372,12 +370,6 @@ void tud_cdc_tx_complete_cb(uint8_t itf)
 
 int main(void)
 {
-	vreg_set_voltage(VREG_VOLTAGE_1_30);
-	set_sys_clock_khz(266000, true);
-
-	uint32_t freq = clock_get_hz(clk_sys);
-	clock_configure(clk_peri, 0, CLOCKS_CLK_PERI_CTRL_AUXSRC_VALUE_CLK_SYS, freq, freq);
-
 	stdio_init_all();
 
 	gpio_init(LED_PIN);
